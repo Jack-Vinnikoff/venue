ba6ca037a465394dbb35e1b030ffb836eeedcc6e
Jekash <vinnicox@gmail.com>
2018-09-11 00:57:30 +0300

    modified gitignore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header_footer/Header.jsx'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ height: "1500px", background: 'cornFlowerblue'}}>
         <Header/>
       </div>
     );
---
+++ src/components/Header_footer/SideDrawer.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+const SideDrawer = (props) => {
+    return (
+        <Drawer
+            anchor="right"
+            open={props.open}
+            onClose={()=>props.onClose(false)}
+        >
+
+            <List component="nav">
+                <ListItem button onClick={() => console.log('staert')}>
+                    Event Starts in
+                </ListItem>
+                <ListItem button onClick={() => console.log('staert')}>
+                    VENUE NFO
+                </ListItem>
+                <ListItem button onClick={() => console.log('staert')}>
+                    Highlights
+                </ListItem>
+                <ListItem button onClick={() => console.log('staert')}>
+                    Pricing
+                </ListItem>
+                <ListItem button onClick={() => console.log('staert')}>
+                    Location
+                </ListItem>
+            </List>
+        </Drawer>
+    )
+}
+export default SideDrawer
