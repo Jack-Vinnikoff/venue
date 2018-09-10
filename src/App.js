e953021e127e17078f742a20c77ce483e8d6c1c5
Jekash <vinnicox@gmail.com>
2018-09-10 22:52:51 +0300

    add logo header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './resources/styles.css'
+
+import Header from './components/Header_footer/Header.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
