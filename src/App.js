d209633a9a13a2d1ac320a3ed1ee4644feb8cc95
Jekash <vinnicox@gmail.com>
2018-09-17 00:54:27 +0300

    created slider carousel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './resources/styles.css'
 
 import Header from './components/Header_footer/Header.jsx'
-
+import Featured from './components/Featured'
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height: "1500px", background: 'cornFlowerblue'}}>
         <Header/>
+        <Featured />
       </div>
     );
   }
