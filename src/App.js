240284e4ee0f2d65f70f9bf7309a65f3f526a58e
Jekash <vinnicox@gmail.com>
2018-09-22 12:49:53 +0300

    created block Highlights

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './resources/styles.css'
 import Header from './components/Header_footer/Header.jsx'
 import Featured from './components/Featured'
 import VenueNfo from './components/VenueNfo'
+import Highlights from './components/Highlights'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: 'cornFlowerblue'}}>
-        <Header/>
+      <div className="App" style={{ height: "1500px"}}>
+        <Header />
         <Featured />
         <VenueNfo />
+        <Highlights />
       </div>
     );
   }
---
+++ src/components/Highlights/Discount.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Fade, Slide } from 'react-reveal'
+
+class Discount extends Component {
+
+    state = {
+        discountStart: 0,
+        discountEnd: 30,
+    }
+
+    componentDidUpdate() {
+        setTimeout(() => {
+            this.porcentage()
+        }, 30)
+    }
+
+    porcentage = () => {
+        if (this.state.discountStart < this.state.discountEnd) {
+            this.setState({
+                discountStart: this.state.discountStart +1,
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                    <Fade
+                        onReveal={() => this.porcentage()}
+                    >
+                        <div className="discount_porcentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase tickets before 20th December</h3>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                            <div>
+                                Button
+                            </div>
+                        </div>
+                    </Slide>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Discount
